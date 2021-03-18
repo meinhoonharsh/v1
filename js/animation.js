d668c2f1b5023e44a4ed0c92ddb8ed1da495baf8
Harsh Vishwakarma <harshprogrammer782@gmail.com>
2021-03-18 20:23:20 +0530

    Initial Commit

---
+++ js/animation.js
@@ -0,0 +1,347 @@
+ 
+
+
+
+
+
+ $(window).on("load",function(){
+        var perc="50%";
+        /////////animation
+        $("#aboutup").waypoint(function(direction){
+          $("#aboutup").addClass(" animate__animated animate__zoomIn")
+         },{
+          offset:perc
+        });
+
+
+
+        /////////animation
+        $("#hackerimg").waypoint(function(direction){
+          $("#hackerimg").addClass(" animate__animated animate__fadeInLeft")
+         },{
+          offset:perc
+        });
+
+
+
+        /////////animation
+        $("#userabout").waypoint(function(direction){
+          $("#userabout").addClass(" animate__animated animate__fadeInRight")
+         },{
+          offset:perc
+        });
+
+
+
+
+        /////////animation
+        $("#opup").waypoint(function(direction){
+          $("#opup").addClass(" animate__animated animate__fadeInUp");
+          $("#opdown").addClass(" animate__animated animate__fadeInUp");
+         },{
+          offset:perc
+        });
+
+
+
+
+        /////////animation
+        $("#servicesup").waypoint(function(direction){
+          $("#servicesup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+        /////////animation
+        $("#contactusleft").waypoint(function(direction){
+          $("#contactusleft").addClass(" animate__animated animate__fadeInLeft")
+         },{
+          offset:perc
+        });
+
+        /////////animation
+        $("#contactusright").waypoint(function(direction){
+          $("#contactusright").addClass(" animate__animated animate__fadeInRight")
+         },{
+          offset:perc
+        });
+
+        /////////animation
+        $(".servicediv").waypoint(function(direction){
+          $(".servicediv").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+      
+
+        /////////animation
+        $("#skillup").waypoint(function(direction){
+          $("#skillup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#skilldown").waypoint(function(direction){
+          $("#skilldown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+
+      
+
+        /////////animation
+        $("#languageup").waypoint(function(direction){
+          $("#languageup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#languagedown").waypoint(function(direction){
+          $("#languagedown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+      
+
+        /////////animation
+        $("#learningup").waypoint(function(direction){
+          $("#learningup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#learningdown").waypoint(function(direction){
+          $("#learningdown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+      
+
+        /////////animation
+        $("#frameworkup").waypoint(function(direction){
+          $("#frameworkup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#frameworkdown").waypoint(function(direction){
+          $("#frameworkdown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+        /////////animation
+        $("#recentprojectup").waypoint(function(direction){
+          $("#recentprojectup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#recentprojectdown").waypoint(function(direction){
+          $("#recentprojectdown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+        /////////animation
+        $("#topprojectup").waypoint(function(direction){
+          $("#topprojectup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#topprojectdown").waypoint(function(direction){
+          $("#topprojectdown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+
+        /////////animation
+        $("#allprojectup").waypoint(function(direction){
+          $("#allprojectup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#allprojectdown").waypoint(function(direction){
+          $("#allprojectdown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+
+
+
+
+
+
+        /////////animation
+        $("#wdup").waypoint(function(direction){
+          $("#wdup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#wddown").waypoint(function(direction){
+          $("#wddown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+        /////////animation
+        $("#hdup").waypoint(function(direction){
+          $("#hdup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#hddown").waypoint(function(direction){
+          $("#hddown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+        /////////animation
+        $("#psdup").waypoint(function(direction){
+          $("#psdup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#psddown").waypoint(function(direction){
+          $("#psddown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+        /////////animation
+        $("#wordpressup").waypoint(function(direction){
+          $("#wordpressup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#wordpressdown").waypoint(function(direction){
+          $("#wordpressdown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+        /////////animation
+        $("#wixup").waypoint(function(direction){
+          $("#wixup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#wixdown").waypoint(function(direction){
+          $("#wixdown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+
+
+
+        /////////animation
+        $("#contactformup").waypoint(function(direction){
+          $("#contactformup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#contactformdown").waypoint(function(direction){
+          $("#contactformdown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+
+
+
+        /////////animation
+        $("#socialpageup").waypoint(function(direction){
+          $("#socialpageup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#socialpagedown").waypoint(function(direction){
+          $("#socialpagedown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+
+
+
+        /////////animation
+        $("#subscribeup").waypoint(function(direction){
+          $("#subscribeup").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+        /////////animation
+        $("#subscribedown").waypoint(function(direction){
+          $("#subscribedown").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+
+
+
+        /////////animation
+        $("#maplocation").waypoint(function(direction){
+          $("#maplocation").addClass(" animate__animated animate__fadeInUp")
+         },{
+          offset:perc
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      });
+
+
+
+
+
+
+
+
+
+
+
